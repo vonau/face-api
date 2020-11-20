2032f8120b04c471dec65776dc44ca6ac90d707f
Nico Vonau <vonaun@student.ethz.ch>
2020-11-20 14:40:29 +0100

    change port

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => {handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {handleApiCall(req, res)})
 
 
-app.listen(3000, ()=> {
-	console.log('app is running on port 3k')
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port ${process.env.PORT}`)
 })
